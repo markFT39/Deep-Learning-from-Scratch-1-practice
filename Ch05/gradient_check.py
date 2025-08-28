de75b6dcb550ee417b73f6a022013d28adcaa611
markFT39 <43339545+markFT39@users.noreply.github.com>
2025-08-28 13:35:18 +0900

    [Ch06] 매개변수 갱신 방법론 (feat)
    - SGD, 확률적경사하강법 학습 및 구현
    - 모멘텀 학습 및 구현
    - AdaGrad 학습 및 구현
    - Adam 학습 및 사용
    - 갱신 방법의 결과 도출 및 비교

--- Ch05/gradient_check.py
+++ Ch05/gradient_check.py
@@ -13,8 +13,8 @@ network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 x_batch= x_train[:3]
 t_batch = t_train[:3]
 
-grad_numerical = network.numerical_gradient(x_batch, t_batch)
-grad_backdrop = network.gradient(x_batch, t_batch)
+grad_numerical = network.numerical_gradient(x_batch, t_batch)   # 수지적 미분값
+grad_backdrop = network.gradient(x_batch, t_batch)              # 오차역전파법의 해석적 미분값
 
 # 각 가중치의 차이의 절댓값을 구한 후, 그 절댓값들의 평균을 낸다.
 # 수치적 미분과 해석적 미분의 각 구현의 결과를 비교하여 그 차이가 매우 작음을 확인하고 오차역전파법 구현에 오류가 없는지 검증할 수 있다.
