2c45751980fd6b1033a876fd63714b526597e25b
markFT39 <43339545+markFT39@users.noreply.github.com>
2025-09-27 16:48:36 +0900

    [3-Ch01] Step01 ~ Step05 미분 자동 계산
    - 개발 환경 설정 (gitignore 설정 업데이트 등)
    - 변수, 함수, 함수 연결, 미분 및 역전파 개념 학습
    - 프레임워크의 기본 구현 (변수, 함수, 미분)

---
+++ Deep Learning from Scratch (3)/steps/step02.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)     # 구제척인 계산은 forward에서 진행
+        output = Variable(y)
+        return output
+    
+    def forward(self, x):
+        raise NotImplementedError()
+    
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+    
+if __name__ == '__main__':
+    x = Variable(np.array(10))
+    f = Square()
+    y = f(x)
+    print(type(y))
+    print(y.data)
